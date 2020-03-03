30411d95207091d252cc7483c17e841b4c0f0f36
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-03-03 19:18:59 +0200

    Ninja Lvl 1

---
+++ 002_ninja_level_1/exercise_001/exercise_1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("Value of x", x, "Value of y", y, "Value of z", z)
+
+	fmt.Println("Value of x", x)
+	fmt.Println("Value of y", y)
+	fmt.Println("Value of z", z)
+}
